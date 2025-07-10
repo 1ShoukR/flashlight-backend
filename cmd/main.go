047ba55c41152c11cfeb28f25c6bcf319d9dccc2
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-09 22:43:19 -0400

    execute server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/1ShoukR/flashlight-backend/internal/server"
+)
 
 func main() {
     fmt.Println("Hello, World!")
+    err := server.NewServer().ListenAndServe()
+
+    if err != nil {
+        panic(fmt.Sprintf("cannot start server: %s", err))
+    }
 }
