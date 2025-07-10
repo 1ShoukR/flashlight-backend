488aea85132c42713c8faab6e432eef0527d6e2d
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-09 22:43:46 -0400

    created NewServer instance to be used in main.go

---
+++ internal/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	port   int        
+	db     *gorm.DB   
+	router *gin.Engine 
+}
+
+func NewServer() *http.Server {
+	_ = godotenv.Load()
+	port, _ := strconv.Atoi(os.Getenv("PORT")) 
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	router := gin.Default()
+	newServer := &Server{
+		port:   port,    
+		db:     db,      
+		router: router,
+	}
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", newServer.port),
+		Handler:      newServer.router,
+	}
+	return server
+}
+
