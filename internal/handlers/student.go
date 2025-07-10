34750e8bcff09e6c1fb967aa5205da9a30492bdc
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-10 16:11:19 -0400

    added CreateStudent and GetAllStudents logic

--- internal/handlers/student.go
+++ internal/handlers/student.go
@@ -1,14 +1,50 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/1ShoukR/flashlight-backend/internal/models"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func IsThisWorking(db *gorm.DB, router *gin.Engine) gin.HandlerFunc {
+type UserData struct {
+	Name 	string		`json:"name" binding:"required"`
+	Grade	uint		`json:"grade" binding:"required"`
+}
+
+func GetAllStudents(db *gorm.DB, router *gin.Engine) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"success": "This is working! "})
+		var students []models.Student
+		if err := db.Find(&students); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch students"})
+			return
+		}
+		if len(students) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "No students found"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"success": "This is working! ", "students": students})
+	}
+}
+
+func CreateStudent(db *gorm.DB, router *gin.Engine) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req UserData
+		if err := c.ShouldBindJSON(&req); err != nil {
+			errString := fmt.Sprintf("Error binding JSON: %v", err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": errString})
+			return
+		}
+		var newUser models.Student
+		newUser.Grade = req.Grade
+		newUser.Name = req.Name
+		if err := db.Create(&newUser).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create student"})
+			return
+		}
+		c.JSON(200, gin.H{"status": "success", "message": "user has bee created!", "data": newUser})
+		
 	}
 }
