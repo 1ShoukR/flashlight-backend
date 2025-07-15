cc3bd742edee019a5b486894b084085c0471c3c1
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-15 16:02:27 -0400

    removed restrictions due to table restrictions

--- internal/models/student.go
+++ internal/models/student.go
@@ -1,7 +1,6 @@
 package models
-
 type Student struct {
-	StudentID 		uint		`gorm:"primaryKey;autoIncrement:true"`
-	Name			string		`gorm:"size:50;not null"`
-	Grade			uint		`gorm:"size:2;not null"`
+    StudentID uint   `gorm:"primaryKey;autoIncrement"` 
+    Name      string `gorm:"size:50;not null"`
+    Grade     uint   `gorm:"not null"`  
 }
